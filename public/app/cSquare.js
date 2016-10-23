c6610851bfdd75a635b755dc24367f2f3e2f8e2c
Thomics <contot@gmail.com>
2016-10-22 22:07:36 -0700

    updates to drag and then drop

---
+++ public/app/cSquare.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Displays the chessboard square.
+ * @display
+ **/
+
+angular.module('chess')
+  .directive('cSquare', function(){
+    return {
+      restrict: 'A',
+      scope: {
+
+      }
+    };
+  });
--- public/app/chess.controller.js
+++ public/app/chess.controller.js
@@ -33,9 +33,10 @@
     }
 
 
-    function Square(row, column, occupied, piece) {
+    function Square(row, column, squareNum, occupied, piece) {
       this.row = row;
       this.column = column;
+      this.squareNum = squareNum;
       this.occupied = occupied;
       this.piece = piece;
     }
@@ -44,14 +45,17 @@
     function createBoard() {
       var chessSquare;
       var chessRow = [];
+      var squareNum = 1;
       for ( var i = 0; i <= 7; i++ ) {
         for ( var j = 0; j <= 7; j++ ) {
+
           if ( i <= 1 || i >= 6 ) {
-            chessSquare = setPieces(i, j);
+            chessSquare = setPieces(i, j, squareNum);
           } else {
-            chessSquare = new vm.Square(i, j, false, '');
+            chessSquare = new vm.Square(i, j, squareNum, false, '');
           }
           chessRow.push(chessSquare);
+          squareNum++;
         }
         vm.chessboard.push(chessRow);
         chessRow = [];
@@ -62,34 +66,34 @@
 
 
 
-    function setPieces(i, letter) {
+    function setPieces(i, letter, squareNum) {
       var color = (i >= 6) ? 'white' : 'black';
       var pieceLetter;
 
       if ( i === 1 || i === 6 ) {
         pieceLetter = (color === 'white') ? 'p' : 'o'; //Pawn White p, Black o
         var piece = new Pawn(i, letter, color, pieceLetter);
-        return new vm.Square(i, letter, true, piece);
+        return new vm.Square(i, letter, squareNum, true, piece);
 
       } else if ( letter === 0 || letter === 7 ) {
         pieceLetter = (color === 'white') ? 'r' : 't'; //Rook White r, Black t
-        return new vm.Square(i, letter, true, {piece: 'rook', color: color, pieceLetter: pieceLetter});
+        return new vm.Square(i, letter, squareNum, true, {piece: 'rook', color: color, pieceLetter: pieceLetter});
 
       } else if ( letter === 1 || letter === 6) {
         pieceLetter = (color === 'white') ? 'h' : 'j'; //Knight White h, Black j
-        return new vm.Square(i, letter, true, {piece: 'knight', color: color, pieceLetter: pieceLetter});
+        return new vm.Square(i, letter, squareNum, true, {piece: 'knight', color: color, pieceLetter: pieceLetter});
 
       } else if ( letter === 2 || letter === 5) {
         pieceLetter = (color === 'white') ? 'b' : 'n'; //Bishop White b, Black n
-        return new vm.Square(i, letter, true, {piece: 'bishop', color: color, pieceLetter: pieceLetter});
+        return new vm.Square(i, letter, squareNum, true, {piece: 'bishop', color: color, pieceLetter: pieceLetter});
 
       } else if ( letter === 3) {
         pieceLetter = (color === 'white') ? 'q' : 'w'; //Queen White q, Black w
-        return new vm.Square(i, letter, true, {piece: 'queen', color: color, pieceLetter: pieceLetter});
+        return new vm.Square(i, letter, squareNum, true, {piece: 'queen', color: color, pieceLetter: pieceLetter});
 
       } else {
         pieceLetter = (color === 'white') ? 'k' : 'l'; //King White k, Black l
-        return new vm.Square(i, letter, true, {piece: 'king', color: color, pieceLetter: pieceLetter});
+        return new vm.Square(i, letter, squareNum, true, {piece: 'king', color: color, pieceLetter: pieceLetter});
       }
 
     }
@@ -104,7 +108,6 @@
       this.pieceLetter = letter;
       this.moved = false;
       this.move = function() {
-        console.log(this);
 
         movePiece(this);
 
@@ -113,12 +116,20 @@
 
     }
 
-    function movePiece(piece, destination) {
+    function movePiece(piece) {
+
+      //Move the piece to the new spot
+
+
+      vm.chessboard[piece.row][piece.column].piece = vm.chessboard[piece.row--][piece.column].piece;
+      vm.chessboard[piece.row][piece.column].piece = piece;
+
 
       console.log(vm.chessboard[piece.row][piece.column]);
+
+
+
     }
-
-
 
 
 
