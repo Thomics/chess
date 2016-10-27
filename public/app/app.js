4816d0de58dffea5caa1fd19687df9f042cb93b6
Thomics <contot@gmail.com>
2016-10-26 20:12:55 -0700

    got the previous square

--- public/app/app.js
+++ public/app/app.js
@@ -4,12 +4,14 @@ var vm = this;
 
 vm.chessboard = [];
 vm.createBoard = createBoard;
+vm.pieceList = {};
 vm.Square = Square;
 vm.activate = activate;
 vm.generateHtml = generateHtml;
 vm.allowDrop = allowDrop;
 vm.drag = drag;
 vm.drop = drop;
+vm.movePiece = movePiece;
 
 
 ///////////////////////////////
@@ -36,7 +38,6 @@ function Pawn(id, color, letter) {
   this.color = color;
   this.pieceLetter = letter;
   this.previousSquares = [id];
-  this.lastSquare = id;
   this.moved = false;
 
 }
@@ -67,40 +68,34 @@ function setPieces(i, j, squareNum) {
   if ( i >= 48 ) { var color = 'white'; }
 
   var pieceLetter = '';
-
+  var piece = {};
   if ( (i >= 8 && i <= 15) || (i >= 48 && i <= 55) ) {
     pieceLetter = (color === 'white') ? 'p' : 'o'; //Pawn White p, Black o
-    var piece = new Pawn(i, color, pieceLetter);
-    return new vm.Square(i, true, piece);
-
+    piece = new Pawn(i, color, pieceLetter);
   } else if ( i === 0 || i === 7 || i === 56 || i === 63 ) {
     pieceLetter = (color === 'white') ? 'r' : 't'; //Rook White r, Black t
-    return new vm.Square(i, true, {piece: 'rook', color: color, pieceLetter: pieceLetter});
-
+    piece = {piece: 'rook', color: color, pieceLetter: pieceLetter};
   } else if ( i === 1 || i === 6 || i === 57 || i === 62 ) {
     pieceLetter = (color === 'white') ? 'h' : 'j'; //Knight White h, Black j
-    return new vm.Square(i, true, {piece: 'knight', color: color, pieceLetter: pieceLetter});
-
+    piece = {piece: 'knight', color: color, pieceLetter: pieceLetter};
   } else if ( i === 2 || i === 5 || i === 58 || i === 61 ) {
     pieceLetter = (color === 'white') ? 'b' : 'n'; //Bishop White b, Black n
-    return new vm.Square(i, true, {piece: 'bishop', color: color, pieceLetter: pieceLetter});
-
+    piece = {piece: 'bishop', color: color, pieceLetter: pieceLetter};
   } else if ( i === 3 || i === 59 ) {
     pieceLetter = (color === 'white') ? 'q' : 'w'; //Queen White q, Black w
-    return new vm.Square(i, true, {piece: 'queen', color: color, pieceLetter: pieceLetter});
-
+    piece = {piece: 'queen', color: color, pieceLetter: pieceLetter}
   } else if (i === 4 || i == 60) {
     pieceLetter = (color === 'white') ? 'k' : 'l'; //King White k, Black l
-    return new vm.Square(i, true, {piece: 'king', color: color, pieceLetter: pieceLetter});
+    piece = {piece: 'king', color: color, pieceLetter: pieceLetter}
   } else {
-
-    return new vm.Square(i, false, {piece: '', color: '', pieceLetter: ''});
+    piece = {piece: '', color: '', pieceLetter: ''};
+    return new vm.Square(i, false, piece);
   }
 
+  vm.pieceList[i] = piece;
+  return new vm.Square(i, true, piece);
+
 }
-
-
-
 
 
 function allowDrop(ev) {
@@ -108,85 +103,66 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+  var lastSquare = ev.path[1].classList[1];
 
   ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData('previousSquare', lastSquare);
   ev.target.style.opacity = ".99";
 }
 
 
 
 function drop(ev) {
-  console.log(ev);
+  //console.log(ev);
 
   ev.preventDefault();
 
   //The ID of the moved piece
-  var piece = ev.dataTransfer.getData("text");
-
-  //Gets the square that the element came from.
-  var previousSquare = vm.chessboard[Number(piece)];
-  console.log(previousSquare);
-
+  var pieceId = Number(ev.dataTransfer.getData('text'));
+  //The square the piece just moved from.
+  var previousSquare = Number(ev.dataTransfer.getData('previousSquare'));
 
   //Get the target square whether the user takes another piece or places it in an empty square.
   var targetSquare = ev.target.classList.length === 0 ? ev.target.parentElement : ev.target;
 
   //The class/id of the target square.
-  var newSquare = targetSquare.classList[1] || '';
+  var newSquare = Number(targetSquare.classList[1]) || -1;
 
-  //console.log(targetSquare);
-
-
-  targetSquare.innerHTML = '';
 
   //Adds the piece to the target square.
-  targetSquare.appendChild(document.getElementById(piece));
+  targetSquare.appendChild(document.getElementById(pieceId));
 
-  movePiece(Number(newSquare), Number(piece), previousSquare);
+  vm.movePiece(newSquare, previousSquare, pieceId);
 
   ev.dataTransfer.clearData();
 
 }
 
 //newSquare - the id/class of the new square.
+//previousSquare - the previous squares number.
 //pieceID - the id of the piece that was moved.
-function movePiece(newSquare, pieceId, previousSquare) {
+function movePiece(newSquare, previousSquare, pieceId) {
 
-  console.log(newSquare);
-  console.log(pieceId);
-  console.log(previousSquare);
+  console.log(newSquare + ' new sq');
+  console.log(pieceId + ' piece id');
+  console.log(previousSquare + ' previous sq');
 
+  var pieceObj = vm.pieceList[pieceId]
 
-  var squareId = vm.chessboard[newSquare].lastSquare;
-
-
-  //The square object that holds the square info and the piece object.
-  var pieceObj = vm.chessboard[pieceId].piece;
-  //console.log(pieceObj);
-
-
-  //Gets the value of the pieces most recent square location.
-  var oldSquare = pieceObj.lastSquare;
-
-  //Sets the value of the pieces most recent square to the current square.
-  pieceObj.lastSquare = newSquare;
-
-  //Adds the previous square to the list of squares the piece has been on.
+  //Adds the square the piece moved from to the list of previous squares.
   pieceObj.previousSquares.push(newSquare);
-  console.log(pieceObj.previousSquares.length);
-
-
   pieceObj.moved = true;
+
+  console.log(pieceObj);
+
 
   //Sets the new square to occupied, the old square to not occupied.
   vm.chessboard[newSquare].occupied = true;
-  vm.chessboard[oldSquare].occupied = false;
+  vm.chessboard[previousSquare].occupied = false;
 
   //Moves the piece from one square to another (objects).
   vm.chessboard[newSquare].piece = pieceObj;
-  vm.chessboard[oldSquare].piece = {piece: '', color: '', pieceLetter: ''};
-  console.log(vm.chessboard[newSquare]);
-  console.log(vm.chessboard[oldSquare]);
+  vm.chessboard[previousSquare].piece = {piece: '', color: '', pieceLetter: ''};
 
 
 }
